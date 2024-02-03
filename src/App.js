746f501ce1ae860bf2b0995b2b7601e513538a81
unknown <iprakritip@gmail.com>
2024-02-03 09:48:20 +0545

    setup tailwind

--- src/App.js
+++ src/App.js
@@ -2,8 +2,12 @@
 
 function App() {
   return (
-    <div className="App">
-      Hello
+    <div className="w-screen h-screen bg-red-500 flex justify-center items-center">
+      <div className="w-1/3 h-2/3 bg-white border rounded-md">
+        
+        <p>Will you be my Valentine?</p>
+
+      </div>
     </div>
   );
 }
